ff2e137bbeb5b5894a3a4f68d2e713a2228504e1
MasoodTadi <m.tadi.ie@gmail.com>
2022-05-17 23:46:16 +0200

    pp1:32

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N mnist_rsvd
-#PBS -l select=4:mpiprocs=128,walltime=00:10:00
+#PBS -l select=1:mpiprocs=64,walltime=00:10:00
 #PBS -q qexp
 #PBS -e mnist_rsvd.e
 #PBS -o mnist_rsvd.o
@@ -14,4 +14,4 @@ echo "loaded R"
 ## Fix for warnings from libfabric/1.12 bug
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
-time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:32:node Rscript mnist_rsvd.R
